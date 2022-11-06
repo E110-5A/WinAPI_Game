ffb3c5dc7dc9fe50e5522784686257bf9b8806e6
0_5 <ell0_5a@outlook.com>
2022-11-07 02:03:16 +0900

    플레이어 애니메이션 추가

--- WindowsAPI/jsPlayer.cpp
+++ WindowsAPI/jsPlayer.cpp
@@ -37,7 +37,6 @@ namespace js
 
 	void Player::Init()
 	{
-		// 내 이미지 세팅
 		if (nullptr == mImage)
 		{
 			mImage = Resources::Load<Image>
@@ -45,24 +44,48 @@ namespace js
 		}
 
 		// 콜라이더 설정
-		Collider* myCollider = new Collider;
-		myCollider->SetPos(GetPos());
-		myCollider->SetScale(Size(5.f, 12.f) * GetScale());
-		myCollider->SetOffset(Vector2(-12.f, 0.f));
-		AddComponent(myCollider);
-			
+		mCollider = new Collider;
+		mCollider->SetPos(GetPos());
+		mCollider->SetScale(Size(7.f, 12.f) * GetScale());
+		mCollider->SetOffset(Vector2(-10.f, 0.f));
+		AddComponent(mCollider);
+
 		// 애니메이터 설정
+		InitAnim();
+
+		mAnimator->Play(L"IdleR");
+	}
+
+	void Player::InitAnim()
+	{
 		mAnimator = new Animator;
-		mAnimator->CreateAnimation(L"Idle", mImage, Pos(0.f, 0.f), Size(30.f, 36.f)
-			, Vector2(0.f, 0.f), 1, 0.1f);
-		mAnimator->CreateAnimation(L"WalkRight", mImage, Pos(0.0f, 36.0f), Size(18.0f, 33.0f)
-			, Vector2(0.0f, 0.0f), 8, 0.1f);
-		mAnimator->CreateAnimation(L"DubleTab", mImage, Pos(0.0f, 69.0f), Size(60.0f, 36.0f)
-			, Vector2(0.0f, 0.0f), 5, 0.1f);
-
-		mAnimator->Play(L"Idle");
-
 		AddComponent(mAnimator);
+
+		mAnimator->CreateAnimation(L"IdleR", mImage, Pos(0.f, 0.f), Size(30.f, 36.f)
+			, Vector2(-10.f, 0.f), 1, 0.1f);
+		mAnimator->CreateAnimation(L"IdleL", mImage, Pos(0.f, 36.f), Size(30.f, 36.f)
+			, Vector2(-10.f, 0.f), 1, 0.1f);
+
+		mAnimator->CreateAnimation(L"WalkR", mImage, Pos(0.0f, 72.0f), Size(18.0f, 33.0f)
+			, Vector2(-10.0f, 0.0f), 8, 0.1f);
+		mAnimator->CreateAnimation(L"WalkL", mImage, Pos(0.0f, 105.0f), Size(18.0f, 33.0f)
+			, Vector2(-10.0f, 0.0f), 8, 0.1f);
+
+
+		mAnimator->CreateAnimation(L"DubleTabR", mImage, Pos(0.0f, 138.0f), Size(60.0f, 36.0f)
+			, Vector2(2.0f, 0.0f), 5, 0.1f);
+		mAnimator->CreateAnimation(L"DubleTabL", mImage, Pos(0.0f, 174.0f), Size(60.0f, 36.0f)
+			, Vector2(2.0f, 0.0f), 5, 0.1f);
+
+		mAnimator->CreateAnimation(L"FMJR", mImage, Pos(0.0f, 210.0f), Size(96.0f, 33.0f)
+			, Vector2(23.0f, 0.0f), 5, 0.1f);
+		mAnimator->CreateAnimation(L"FMJL", mImage, Pos(0.0f, 243.0f), Size(96.0f, 33.0f)
+			, Vector2(23.0f, 0.0f), 5, 0.1f);
+
+		mAnimator->CreateAnimation(L"DiveR", mImage, Pos(0.0f, 276.0f), Size(36.0f, 36.0f)
+			, Vector2(-12.0f, 0.0f), 9, 0.1f);
+		mAnimator->CreateAnimation(L"DiveL", mImage, Pos(0.0f, 312.0f), Size(36.0f, 36.0f)
+			, Vector2(-12.0f, 0.0f), 9, 0.1f);
 	}
 
 	void Player::Tick()
@@ -71,50 +94,27 @@ namespace js
 
 		Pos pos = GetPos();
 		
-		if (KEY_DOWN(eKeyCode::A))
-		{
-			mAnimator->Play(L"WalkRight", true);
-		}
-		if (KEY_DOWN(eKeyCode::D))
-		{
-			mAnimator->Play(L"WalkRight", true);
-		}
-		if (KEY_DOWN(eKeyCode::J))
-		{
-			mAnimator->Play(L"DubleTab");
-		}
-
-		if (KEY_UP(eKeyCode::A))
-		{
-			mAnimator->Play(L"Idle", true);
-		}
-		if (KEY_UP(eKeyCode::D))
-		{
-			mAnimator->Play(L"Idle", true);
-		}
-		if (KEY_UP(eKeyCode::J))
-		{
-			mAnimator->Play(L"Idle", true);
-		}
-
-		/*
+		// 애니메이션
+		PlayAnim();
+		
+		// 로직
 		if (KEY_PRESSE(eKeyCode::W))
 		{
 			pos.y -= mSpeed * Time::GetDeltaTime();
-		}
-		if (KEY_PRESSE(eKeyCode::A))
-		{
-			pos.x -= mSpeed * Time::GetDeltaTime();
 		}
 		if (KEY_PRESSE(eKeyCode::S))
 		{
 			pos.y += mSpeed * Time::GetDeltaTime();
 		}
-		if (KEY_PRESSE(eKeyCode::D))
+		if (KEY_PRESSE(eKeyCode::LEFT))
+		{
+			pos.x -= mSpeed * Time::GetDeltaTime();
+		}		
+		if (KEY_PRESSE(eKeyCode::RIGHT))
 		{
 			pos.x += mSpeed * Time::GetDeltaTime();
-		}*/
-		/*if (KEY_DOWN(eKeyCode::SPACE))
+		}
+		/*if (KEY_DOWN(eKeyCode::Z))
 		{
 			Projectile* missile = new Projectile;
 			Scene* playScene = SceneManager::GetCurScene();
@@ -127,14 +127,45 @@ namespace js
 			missile->SetPos(missilePos);
 		}*/
 
-		mAnimator->mCompleteEvent = std::bind(&Player::WalkComplete, this);
-
+		//mAnimator->mCompleteEvent = std::bind(&Player::ActionComplete, this);
 		SetPos(pos);
 	}
 	void Player::Render(HDC hdc)
 	{
 
 		GameObject::Render(hdc);
+	}
+	void Player::PlayAnim()
+	{
+		if (KEY_DOWN(eKeyCode::RIGHT))
+		{
+			mAnimator->Play(L"WalkR");
+		}
+		if (KEY_DOWN(eKeyCode::LEFT))
+		{
+			mAnimator->Play(L"WalkL");
+		}
+		if (KEY_DOWN(eKeyCode::Z))
+		{
+			mAnimator->Play(L"DubleTabR");
+		}
+		if (KEY_DOWN(eKeyCode::X))
+		{
+			mAnimator->Play(L"FMJR", false);
+		}
+		if (KEY_DOWN(eKeyCode::C))
+		{
+			mAnimator->Play(L"DiveR", false);
+		}
+
+		if (KEY_UP(eKeyCode::RIGHT))
+		{
+			mAnimator->Play(L"IdleR", true);
+		}
+		if (KEY_UP(eKeyCode::LEFT))
+		{
+			mAnimator->Play(L"IdleL", true);
+		}
 	}
 	void Player::OnCollisionEnter(Collider* other)
 	{
@@ -148,5 +179,9 @@ namespace js
 	void Player::WalkComplete()
 	{
 		// 걸을때 이팩트를 추가 하던가 말던가
+	}
+	void Player::ActionComplete()
+	{
+		mAnimator->Play(L"IdleR", true);
 	}
 }
--- WindowsAPI/jsPlayer.h
+++ WindowsAPI/jsPlayer.h
@@ -5,7 +5,7 @@ namespace js
 {
 	class Image;
 	class Animator;
-
+	class Collider;
 	class Player :
 		public GameObject
 	{
@@ -15,20 +15,26 @@ namespace js
 		~Player();
 
 		void Init();
+		void InitAnim();
+
 		virtual void Tick() override;
 		virtual void Render(HDC hdc) override;
+
+		void PlayAnim();
 
 		virtual void OnCollisionEnter(Collider* other)override;
 		virtual void OnCollisionStay(Collider* other)override;
 		virtual void OnCollisionExit(Collider* other)override;
 
 		void WalkComplete();
+		void ActionComplete();
 
 	private:
 		float mSpeed;
 		Image* mImage;
 
 		Animator* mAnimator;
+		Collider* mCollider;
 	};
 }
 
